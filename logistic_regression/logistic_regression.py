f724196ab53e72449307d87bc70dc655f21997be
Andre Rein <ra@ra23.net>
2018-04-13 10:43:38 +0200

    splitted plotting function both versions for logistic regression now working

--- logistic_regression/logistic_regression.py
+++ logistic_regression/logistic_regression.py
@@ -2,10 +2,9 @@ import numpy
 import matplotlib.pyplot as plt
 import scipy.optimize as op
 
+from plotting_lib import *
 
-#data_tmp = numpy.genfromtxt('/home/ar/Downloads/machine-learning-ex1/ex1/ex1datatest.txt', delimiter=',')
-data = numpy.genfromtxt('./testdata/ex2data2.txt', delimiter=',')
-one = numpy.ones((len(data),1))
+
 
 def feature_mapping(X1,X2):
 	#extract X1 and X2
@@ -42,7 +41,7 @@ def split_x_y(data):
 	
 	return (X,Y)
 
-X,Y=split_x_y(data)
+
 
 def normalize(X):
 	mean=X.mean(axis=0)
@@ -61,13 +60,6 @@ def normalize2(X):
 	
 	return X
 
-#X_normalized=normalize(X)
-X_normalized=X
-
-
-# this simply adds 1's in front of the X
-X_augment=numpy.concatenate((one, X_normalized), axis=1)
-X_mapped=feature_mapping(X[:,0],X[:,1])
 
 
 def get_cost_quadratic(X,Y, theta=[0,0]):
@@ -147,11 +139,7 @@ def graph(formula, x_range):
     plt.plot(x, y)  
     
 
-
-theta=numpy.zeros(((numpy.shape(X_mapped)[1])))
-#theta=[0.0, 0.0, 0.0]
-
-def gradient_descent(X,Y,theta, lossfunc, rounds=1000):
+def gradient_descent(X,Y,theta, lossfunc, rounds=1000, alpha=1, granularity=10):
 	"""
 	This is a self implemented gradient descent function.
 	"""
@@ -160,21 +148,16 @@ def gradient_descent(X,Y,theta, lossfunc, rounds=1000):
 	#theta,cost,costdelta = logistic_gradient_descent_round(X,Y,theta,  0.0)
 	#thetas.append((theta, "start"))
 	for i in range(1,rounds):
-		theta,cost,cost_before, costdelta = logistic_gradient_descent_round(X,Y,theta,  1)
-		costs.append(cost)
-		if i % (rounds/3)==1:
+		theta,cost,cost_before, costdelta = logistic_gradient_descent_round(X,Y,theta,  alpha)
+		if i % (rounds/granularity)==1:
+			costs.append(cost)
 			thetas.append((theta, "round"+str(i)))
 			#plt.plot(i,costs[i-1], "r.")
 			print i, theta, cost, cost_before, costdelta
-			plt.plot(i,cost, "r.")
+			#plt.plot(i,cost, "r.")
 	#thetas.append((theta, "round"+str(i)))
-	plt.show()
-	return theta,thetas
 	
-
-gradient_theta, thetas=gradient_descent(X_mapped ,Y,theta, logistic_loss, 100)
-
-
+	return theta,thetas, costs
 
 def logistic_descent_optimal(X, Y, theta):
 	"""
@@ -185,106 +168,71 @@ def logistic_descent_optimal(X, Y, theta):
 	
 	return Result.x,Result
 
-optimal_theta, res =logistic_descent_optimal(X_mapped,Y, theta)
-print "Calculated theta:", gradient_theta
-print "Optimal theta   :",optimal_theta
-print "Theta difference:",gradient_theta - optimal_theta
+def case_1():
+	#data_tmp = numpy.genfromtxt('/home/ar/Downloads/machine-learning-ex1/ex1/ex1datatest.txt', delimiter=',')
+	data = numpy.genfromtxt('./testdata/ex2data1.txt', delimiter=',')
+	one = numpy.ones((len(data),1))
+	X,Y=split_x_y(data)
 
+	#X_normalized=normalize(X)
+	X_normalized=X
 
-thetas.append((optimal_theta,"optimal"))
-#graph(lambda x: x*theta[2]+x*theta[1]+x*theta[0], range(-1, 3))	
-#plt.plot(X[:,1],Y, "ro")
+	# this simply adds 1's in front of the X
+	X_mapped=numpy.concatenate((one, X_normalized), axis=1)
+	#X_mapped=feature_mapping(X[:,0],X[:,1])
+	theta=numpy.zeros(((numpy.shape(X_mapped)[1])))
+	#theta=[0.0, 0.0, 0.0]
+	gradient_theta, thetas, costs=gradient_descent(X_mapped ,Y,theta, logistic_loss, rounds=10000, alpha=0.001, granularity=3)
+	optimal_theta, res =logistic_descent_optimal(X_mapped,Y, theta)
+	print "Calculated theta:", gradient_theta
+	print "Optimal theta   :",optimal_theta
+	print "Theta difference:",gradient_theta - optimal_theta
 
-#plt.show()	
+	thetas.append((optimal_theta,"optimal"))
+	#graph(lambda x: x*theta[2]+x*theta[1]+x*theta[0], range(-1, 3))	
+	#plt.plot(X[:,1],Y, "ro")
 
-def plot_data_scatterplot(X, y, thetas=[]):
-    """Plots data as a scatterplot, with contour lines for thetas.
-    X: (k, 2) data items.
-    y: (k, 1) result (+1 or 0) for each data item in X.
-    thetas: list of (theta array, label) pairs to plot contours.
-    Plots +1 data points as a green x, -1 as red o.
-    """
-    fig, ax = plt.subplots()
-    fig.set_tight_layout(True)
+	#plt.show()	
+	#plot_contour(X,Y, thetas, costs)
+	plot_data_scatterplot(X_mapped,Y,thetas, costs)
+	#print X_augment
+	#print logistic_loss((numpy.array([1,45,85])), optimal_theta)
+	#plot_data_scatterplot(X_augment,Y,[thetas[len(thetas)-1]])
 
-    pos=[]
-    neg=[]
+	#theta=[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+	#cost= get_cost(X,Y,theta)
+	#print cost
+	#print get_cost([[1, 2], [1,3], [1, 4], [1, 5]], [7,6,5,4], [0.1,0.2] )
 
-    pos = [(X[k, 1], X[k, 2]) for k in range(X.shape[0]) if y[k] == 1]
-    neg = [(X[k, 1], X[k, 2]) for k in range(X.shape[0]) if y[k] == 0]
+def case_2():
+	#data_tmp = numpy.genfromtxt('/home/ar/Downloads/machine-learning-ex1/ex1/ex1datatest.txt', delimiter=',')
+	data = numpy.genfromtxt('./testdata/ex2data2.txt', delimiter=',')
+	one = numpy.ones((len(data),1))
+	X,Y=split_x_y(data)
 
-    ax.scatter(*zip(*pos), c='darkgreen', marker='x', linewidths=0)
-    ax.scatter(*zip(*neg), c='red', marker='o', linewidths=0)
+	X_mapped=feature_mapping(X[:,0],X[:,1])
+	theta=numpy.zeros(((numpy.shape(X_mapped)[1])))
+	#theta=[0.0, 0.0, 0.0]
+	gradient_theta, thetas, costs=gradient_descent(X_mapped ,Y,theta, logistic_loss, rounds=10000, alpha=1, granularity=10)
+	optimal_theta, res =logistic_descent_optimal(X_mapped,Y, theta)
+	print "Calculated theta:", gradient_theta
+	print "Optimal theta   :",optimal_theta
+	print "Theta difference:",gradient_theta - optimal_theta
 
-    colors = iter(('blue', 'purple', 'black', 'red', 'green', 'orange', "#AABBCC", "#AA22CC",
-     "#AA44CC", "#AACCCC", "#32BBCC", "#456CCF", "#AA44CC", "#AACCCC",
-     "#AAFF4CC", "#FFC4CC", "#322B2C", "#4565F", "#A344CC", "#A321CC"))
-    
-    contours = []
-    for theta, _ in thetas:
-    	xmax=round(X.max()+1)
-    	xmin= round(X.min()-1)
-        xs = numpy.linspace(xmin, xmax, 200)
-        ys = numpy.linspace(xmin, xmax, 200)
-        xsgrid, ysgrid = numpy.meshgrid(xs, ys)
-        plane = numpy.zeros_like(xsgrid)
-        for i in range(xsgrid.shape[0]):
-            for j in range(xsgrid.shape[1]):
-                plane[i, j] = numpy.array([1, xsgrid[i, j], ysgrid[i, j]]).dot(
-                    theta)
-    	
-        contours.append(ax.contour(xsgrid, ysgrid, plane, colors=colors.next(), levels=[0]))
+	thetas.append((optimal_theta,"optimal"))
+	#graph(lambda x: x*theta[2]+x*theta[1]+x*theta[0], range(-1, 3))	
+	#plt.plot(X[:,1],Y, "ro")
 
-    if thetas:
-        plt.legend([cs.collections[0] for cs in contours],
-                   [label for theta, label in thetas])
-    fig.savefig('binary.png', dpi=80)
-    plt.show()
+	#plt.show()	
+	plot_contour(X,Y, thetas, feature_mapping, costs)
+	#plot_data_scatterplot(X_mapped,Y,thetas, costs)
+	#print X_augment
+	#print logistic_loss((numpy.array([1,45,85])), optimal_theta)
+	#plot_data_scatterplot(X_augment,Y,[thetas[len(thetas)-1]])
 
-def plot_contour(X,Y,theta):
-	u = numpy.linspace(-1, 1.5, 50)
-	v = numpy.linspace(-1, 1.5, 50)
-	
-	pos = numpy.where(Y == 1)
-	neg = numpy.where(Y == 0)
+	#theta=[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+	#cost= get_cost(X,Y,theta)
+	#print cost
+	#print get_cost([[1, 2], [1,3], [1, 4], [1, 5]], [7,6,5,4], [0.1,0.2] )
 
-	plt.scatter(X[pos, 0], X[pos, 1], marker='o', c='b')
-	plt.scatter(X[neg, 0], X[neg, 1], marker='x', c='r')
-	colors = iter(('blue', 'purple', 'black', 'red', 'green', 'orange', "#AABBCC", "#AA22CC",
-     "#AA44CC", "#AACCCC", "#32BBCC", "#456CCF", "#AA44CC", "#AACCCC",
-     "#AAFF4CC", "#FFC4CC", "#322B2C", "#4565F", "#A344CC", "#A321CC"))
-
-	z = numpy.zeros(shape=(len(u), len(v)))
-	contours = []
-	for theta, _ in thetas:
-		for i in range(len(u)):
-			for j in range(len(v)):
-				z[i, j] = (feature_mapping(numpy.array(u[i]), numpy.array(v[j])).dot(theta))
-		contours.append(plt.contour(u, v, z, levels=[0],colors=colors.next()))
-	
-
-
-	plt.title('lambda = %f' % 1)
-	plt.xlabel('Microchip Test 1')
-	plt.ylabel('Microchip Test 2')
-	
-	if thetas:
-		f=[cs.collections[0] for cs in contours]
-		g=[label for theta, label in thetas]
-		
-		plt.legend(f,g)
-		#plt.legend(['y = 1', 'y = 0', 'Decision boundary'])
-	plt.show()
-
-plot_contour(X,Y, thetas)
-#plot_data_scatterplot(X_augment,Y,thetas)
-#print X_augment
-#print logistic_loss((numpy.array([1,45,85])), optimal_theta)
-#plot_data_scatterplot(X_augment,Y,[thetas[len(thetas)-1]])
-
-#theta=[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
-#cost= get_cost(X,Y,theta)
-#print cost
-#print get_cost([[1, 2], [1,3], [1, 4], [1, 5]], [7,6,5,4], [0.1,0.2] )
-
-
+case_2()
---
+++ logistic_regression/plotting_lib.py
@@ -0,0 +1,109 @@
+import numpy
+import matplotlib.pyplot as plt
+import scipy.optimize as op
+
+
+def plot_costs(costs, ax1=None):
+    if ax1 is None:
+        fig,ax1 = plt.subplots(1, 1)
+    for i,c in enumerate(costs):
+        ax1.plot(i,c, "r.")
+        ax1.set_title("Gradient Descent")
+        ax1.set_xlabel("Rounds")
+        ax1.set_ylabel("Cost")
+    
+
+def plot_data_scatterplot(X, y, thetas=[], costs=None):
+    """Plots data as a scatterplot, with contour lines for thetas.
+    X: (k, 2) data items.
+    y: (k, 1) result (+1 or 0) for each data item in X.
+    thetas: list of (theta array, label) pairs to plot contours.
+    Plots +1 data points as a green x, -1 as red o.
+    """
+    #fig, ax = plt.subplots()
+    fig,(ax1, ax2) = plt.subplots(1, 2, sharey=False)
+    fig.set_tight_layout(True)
+
+    pos=[]
+    neg=[]
+
+    pos = [(X[k, 1], X[k, 2]) for k in range(X.shape[0]) if y[k] == 1]
+    neg = [(X[k, 1], X[k, 2]) for k in range(X.shape[0]) if y[k] == 0]
+
+    ax1.scatter(*zip(*pos), c='darkgreen', marker='x', linewidths=0)
+    ax1.scatter(*zip(*neg), c='red', marker='o', linewidths=0)
+
+    colors = iter(('blue', 'purple', 'black', 'red', 'green', 'orange', "#AABBCC", "#AA22CC",
+     "#AA44CC", "#AACCCC", "#32BBCC", "#456CCF", "#AA44CC", "#AACCCC",
+     "#AAFF4CC", "#FFC4CC", "#322B2C", "#4565F", "#A344CC", "#A321CC"))
+    
+    contours = []
+    for theta, _ in thetas:
+    	xmax=round(X.max()+1)
+    	xmin= round(X.min()-1)
+        xs = numpy.linspace(xmin, xmax, 200)
+        ys = numpy.linspace(xmin, xmax, 200)
+        xsgrid, ysgrid = numpy.meshgrid(xs, ys)
+        plane = numpy.zeros_like(xsgrid)
+        for i in range(xsgrid.shape[0]):
+            for j in range(xsgrid.shape[1]):
+                plane[i, j] = numpy.array([1, xsgrid[i, j], ysgrid[i, j]]).dot(
+                    theta)
+    	
+        contours.append(ax1.contour(xsgrid, ysgrid, plane, colors=colors.next(), levels=[0]))
+
+    if thetas:
+        ax1.legend([cs.collections[0] for cs in contours],
+                   [label for theta, label in thetas])
+    
+
+    if costs is not None:
+    	plot_costs(costs, ax2)
+    fig.savefig('binary.png', dpi=80)
+    plt.show()
+
+def plot_contour(X,Y,thetas, feature_mapping=None, costs=None):
+
+	if costs is None:
+		fig, ax1 = plt.subplots(1, 1, sharey=False)
+	else:
+		fig, (ax1, ax2) = plt.subplots(1, 2, sharey=False)
+
+	u = numpy.linspace(-1, 1.5, 50)
+	v = numpy.linspace(-1, 1.5, 50)
+	
+	pos = numpy.where(Y == 1)
+	neg = numpy.where(Y == 0)
+
+	ax1.scatter(X[pos, 0], X[pos, 1], marker='o', c='b')
+	ax1.scatter(X[neg, 0], X[neg, 1], marker='x', c='r')
+	colors = iter(('blue', 'purple', 'black', 'red', 'green', 'orange', "#AABBCC", "#AA22CC",
+     "#AA44CC", "#AACCCC", "#32BBCC", "#456CCF", "#AA44CC", "#AACCCC",
+     "#AAFF4CC", "#FFC4CC", "#322B2C", "#4565F", "#A344CC", "#A321CC"))
+
+	z = numpy.zeros(shape=(len(u), len(v)))
+	contours = []
+	for theta, _ in thetas:
+		for i in range(len(u)):
+			for j in range(len(v)):
+				z[i, j] = (feature_mapping(numpy.array(u[i]), numpy.array(v[j])).dot(theta))
+		contours.append(ax1.contour(u, v, z, levels=[0],colors=colors.next()))
+	
+
+
+	ax1.set_title('lambda = %f' % 1)
+	ax1.set_xlabel('Microchip Test 1')
+	ax1.set_ylabel('Microchip Test 2')
+	
+	if thetas:
+		f=[cs.collections[0] for cs in contours]
+		g=[label for theta, label in thetas]
+		
+		ax1.legend(f,g)
+		#plt.legend(['y = 1', 'y = 0', 'Decision boundary'])
+	
+	if costs is not None:
+		plot_costs(costs, ax2)
+
+	fig.savefig('logistic_classification.png', dpi=80)
+	plt.show()
